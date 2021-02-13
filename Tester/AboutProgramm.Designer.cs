ed7db438dfbe01d304b7dd63f7369c19ba05f05c
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2021-02-13 17:55:23 +0500

    XZ

--- Tester/AboutProgramm.Designer.cs
+++ Tester/AboutProgramm.Designer.cs
@@ -67,7 +67,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(342, 477);
+            this.label2.Location = new System.Drawing.Point(337, 525);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 19);
             this.label2.TabIndex = 2;
