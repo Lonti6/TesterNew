a61116fc42a12d48b4e17275e821e6c0dc99ecc0
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2021-02-17 22:27:46 +0500

    hz

--- Tester/GenerateTable.cs
+++ Tester/GenerateTable.cs
@@ -11,10 +11,6 @@ namespace Tester
     {
         public static void GenerataTable(int countRows, List<string> name, DataGridView dgv) 
         {
-            var column0 = new DataGridViewTextBoxColumn();
-            column0.Width = 30;
-            column0.HeaderText = name[0];
-            dgv.Columns.Add(column0);
             //генерирую таблицу
             var column = new DataGridViewTextBoxColumn();
             column.Width = 30;
@@ -22,12 +18,9 @@ namespace Tester
             dgv.Columns.Add(column);
             for (int i = 1; i<name.Count; i++) 
             {
-<<<<<<< HEAD
-                var column = new DataGridViewTextBoxColumn();
-                column.Width = 55;
-=======
                 column = new DataGridViewTextBoxColumn();
->>>>>>> a488bf8eb4ffaf094669b169312faae8d0cf4b3d
+                column.Width = 55;
+                column = new DataGridViewTextBoxColumn();
                 column.HeaderText = name[i];
                 dgv.Columns.Add(column);
             }
