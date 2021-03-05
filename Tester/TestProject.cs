4cfe1be286a619184fe49454363d86d841c6357b
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2021-03-05 12:30:24 +0500

    тоже самое

--- Tester/TestProject.cs
+++ Tester/TestProject.cs
@@ -8,12 +8,14 @@ namespace Tester
     {
         private Process process;
         private List<string> outputList = new List<string>();
+        private List<string> memoryList = new List<string>();
+        private List<string> timeList = new List<string>();
         /// <summary>
         /// Прогон программы
         /// </summary>
         /// <param name="pathProgram">ссылка на программу</param>
         /// <param name="pathInput">ссылка на входные данные теста</param>
-        public List<string> test(string pathProgram, string pathInput)
+        public List<string>[] test(string pathProgram, string pathInput)
         {
             outputList.Clear();
             string language = pathProgram.Substring(pathProgram.LastIndexOf(".") + 1);
@@ -36,9 +38,12 @@ namespace Tester
                             RedirectStandardInput = true,
                         });
                         process.StandardInput.WriteLine(line);
+                        memoryList.Add(process.PeakWorkingSet64.ToString() + " byte");
+
                         outputList.Add(process.StandardOutput.ReadLine());
                         line = inputTxt.ReadLine();
                         process.Kill();
+                        timeList.Add((process.ExitTime.Millisecond - process.StartTime.Millisecond).ToString());
                     }
                     break;
 
@@ -46,13 +51,13 @@ namespace Tester
                     process = Process.Start(new ProcessStartInfo
                     {
                         FileName = "cmd",
-                        Arguments = "/c cd " + pathProgram.Substring(0, pathProgram.LastIndexOf(@"\")) + " & javac " + file,
+                        Arguments = "/c cd " + pathProgram.Substring(0, pathProgram.LastIndexOf(@"\")) + " & javac " + file + " & exit",
                         CreateNoWindow = true,
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
                         RedirectStandardInput = true,
                     });
-                    process.Kill();
+                    process.WaitForExit();
                     while (line != null)
                     {
                         process = Process.Start(new ProcessStartInfo
@@ -64,11 +69,14 @@ namespace Tester
                             RedirectStandardOutput = true,
                             RedirectStandardInput = true,
                         });
+
                         process.StandardInput.WriteLine(line);
+                        memoryList.Add(process.PeakWorkingSet64.ToString() + " byte");
                         outputList.Add(process.StandardOutput.ReadLine());
                         line = inputTxt.ReadLine();
+                        process.WaitForExit();
                         //process.Kill(); // Этот калл по какой то причине не имеет доступа
-                        process.StandardInput.WriteLine("exit");
+                        timeList.Add((process.ExitTime.Millisecond - process.StartTime.Millisecond).ToString());
                     }
                     File.Delete(pathProgram.Substring(0,pathProgram.LastIndexOf(".")) + ".class" );
                     break;
@@ -85,17 +93,24 @@ namespace Tester
                             RedirectStandardOutput = true,
                             RedirectStandardInput = true,
                         });
+                        //ввод входных данных
                         process.StandardInput.WriteLine(line);
+                        //макс памяти за время использования
+                        memoryList.Add(process.PeakWorkingSet64.ToString() + " byte");
+                        // то что вывела прога
                         outputList.Add(process.StandardOutput.ReadLine());
+                        // след строка входных данных
                         line = inputTxt.ReadLine();
-                        process.Kill();
+                        // проверяем закрылась ли прога
+                        process.WaitForExit();
+                        timeList.Add((process.ExitTime.Millisecond - process.StartTime.Millisecond).ToString());
                     }
                     break;
 
                 default:
+                    var build = new BuildProject().CreateExe(pathProgram);
                     while (line != null)
                     {
-                        var build = new BuildProject().CreateExe(pathProgram);
                         process = Process.Start(new ProcessStartInfo
                         {
                             //FileName = build.CompiledAssembly.FullName.Substring(0, build.CompiledAssembly.FullName.IndexOf(",")),
@@ -105,21 +120,17 @@ namespace Tester
                             RedirectStandardOutput = true,
                             RedirectStandardInput = true,
                         });
-                        //process.BeginOutputReadLine();
                         process.StandardInput.WriteLine(line);
-                        /*process.BeginErrorReadLine();
-                        if (process.StandardError.ReadLine().Length > 0)
-                        {
-                            MessageBox.Show($"Error: {process.StandardError.ReadLine()}");
-                        }*/
+                        memoryList.Add(process.PeakWorkingSet64.ToString() + " byte");
                         outputList.Add(process.StandardOutput.ReadLine());
                         process.Kill();
+                        timeList.Add((process.ExitTime.Millisecond - process.StartTime.Millisecond).ToString());
                         line = inputTxt.ReadLine();
                     }
 
                     break;
             }
-            return outputList;
+            return new List<string>[3]{outputList, memoryList, timeList};
         }
     }
 }
